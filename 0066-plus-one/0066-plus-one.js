00617a781a3a80ec7fb98197f9397723221e2e72
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-03 16:58:57 +0530

    Time: 55 ms (39.46%), Space: 48.5 MB (84.73%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+
+
+        let res = BigInt(digits.join(""))
+        let inc = String(BigInt(res)+1n)
+        //console.log(inc)
+         let t = inc.toString().split("").map(Number)
+        
+        return t
+
+};
