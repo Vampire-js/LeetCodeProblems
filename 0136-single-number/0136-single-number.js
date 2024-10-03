d9876af9669a57f925abbef92c41fb48a315aaac
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-03 17:19:11 +0530

    Time: 77 ms (21.47%), Space: 52.2 MB (41.32%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    nums = nums.sort()
+    for(let i=0; i<nums.length ; i += 2){
+        if( i == nums.length -1 ){
+            return nums[i]
+        }
+        if(nums[i] != nums[i+1]){
+            return nums[i]
+        }
+    }
+};
