942e73ef6396cd715df2b70964fe5aa35aaae4e9
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-06 18:45:36 +0530

    Time: 1937 ms (5.01%), Space: 48.6 MB (71.98%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,25 +3,11 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let left = 0
-    let right = num
-    
-    while(left <= right){
-        let mid = Math.floor((left + right)/2)
-
-        if(mid*mid == num){
-            return mid
-        }
-        if(mid*mid < num){
-            left = mid + 1
-        }else{
-            right = mid -1
-        }
-    
-    }
-    if(left*left == num){
+    if(num == 1) return true
+   for(let i=0; i<=num/2 ; i++){
+    if(i*i == num){
         return true
-    }else{
-        return false
     }
+   }
+   return false
 };
