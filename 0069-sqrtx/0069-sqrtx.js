e38b8eac65094cc2ca6673050170d89d536a6fd4
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-06 13:22:32 +0530

    Time: 74 ms (61.46%), Space: 53.2 MB (13.57%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
+    let left = 0
+    let right = x
+
+    while(left <= right){
+        let mid = Math.floor((right+left)/2)
+        if(mid*mid == x){
+            return mid
+        }
+        if(mid*mid < x){
+            left = mid + 1
+        }else{
+            right = mid - 1
+        }
+    }
+    return right
 };
