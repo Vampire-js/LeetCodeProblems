90ed673f57d687e7f9b963117a14c51f7c698ee1
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-08 23:30:26 +0530

    Time: 124 ms (43.86%), Space: 62 MB (33.92%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.js
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -5,18 +5,20 @@
 var maxPoints = function(points) {
     if (points.length <= 2) return points.length;
 
-    const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
     let max = 1;
+
+
+let findSlope = function (p1, p2) {
+        let [x1,y1] = p1
+        let [x2,y2] = p2
+if(x2-x1 == 0) return "i"
+        return (y2-y1)/(x2-x1)
+    }
 
     for (let i = 0; i < points.length - 1; i++) {
         let map = {};
         let localMax = 0;
-let findSlope = function (p1, p2) {
-        let [x1,y1] = p1
-        let [x2,y2] = p2
 
-        return (y2-y1)/(x2-x1)
-    }
         for (let j = i + 1; j < points.length; j++) {
             const slope = findSlope(points[i], points[j])
             map[slope] = (map[slope] || 0) + 1;
