a3f93171165b388b250d9554032a8a0fd2e969e6
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-08 23:10:39 +0530

    Time: 319 ms (5.26%), Space: 63.4 MB (28.65%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    
+   let findSlope = function (p1, p2) {
+        let num = p2[1] - p1[1]
+        let denom = p2[0] - p1[0]
+    if(denom == 0) return "i"
+        return num/denom
+    }
+    let max = 1
+
+    for(let x =0; x < points.length; x++){
+        let map = {}
+        for(let y = x + 1; y < points.length; y++){
+            let slope = findSlope(points[x], points[y])
+            map[slope] = 1 + (map[slope] || 0 ) 
+    console.log(slope)
+
+        }
+        
+        for (let [slope, value] of Object.entries(map)) {
+            max = Math.max(max, value + 1)
+        }
+    }
+
+   return max
+}
