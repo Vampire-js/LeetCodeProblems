9f6c86ea9876df5ffbff44bf37946b2572c7372d
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-12 09:21:22 +0530

    Time: 57 ms (18.63%), Space: 49.4 MB (7.51%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -11,16 +11,16 @@
  * @param {number} n
  * @return {number}
  */
- var guessNumber = function(n) {
+var guessNumber = function(n) {
     let left = 1;
     let right = n;
 
     while (left <= right) {
-        let mid = left + ((right - left) >> 1);
+        let mid = Math.floor((left + right) / 2);
         let result = guess(mid);
-
+        
         if (result === 0) return mid;
-        else if (result < 0) right = mid - 1;
+        else if (result === -1) right = mid - 1;
         else left = mid + 1;
     }
 };
