aa1cdf54a45d2687a29fd1912a879fa7334fd4c8
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-26 15:46:31 +0530

    Time: 81 ms (25.82%), Space: 53.4 MB (10.24%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,34 +3,16 @@
  * @return {number}
  */
 var reverse = function (x) {
-    let arr = `${x}`.split('')
-
-
-    let left = 0
-    let right = arr.length - 1
-
-    console.log(arr[0])
-    if (arr[0] == "-") {
-        left = 1
+   let ans = 0;
+    if (x < 0) {
+        ans = parseInt(String(x).slice(1).split('').reverse().join('')) * -1;
     } else {
-        left = 0
-    }
-    while (left + 1 <= right) {
-        let a = arr[left]
-        arr[left] = arr[right]
-        arr[right] = a
-
-        left++
-        right--
+        ans = parseInt(String(x).split('').reverse().join(''));
     }
 
+    if (ans > Math.pow(2, 31) - 1 || ans < -Math.pow(2, 31)) {
+        return 0;
+    }
 
-    console.log(arr)
-    arr = parseInt(arr.join(''))
-
-if(Math.abs(arr) >= 2147483648){
-return 0
-}else{
-    return arr
-}
+    return ans;   
 };
