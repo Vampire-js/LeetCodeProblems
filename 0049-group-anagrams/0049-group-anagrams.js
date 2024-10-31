6853440ebc703fc0bd781f253ef37370fe0d614e
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-10-31 16:06:52 +0530

    Time: 29 ms (80.1%), Space: 63.4 MB (42.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,13 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    let ans = {};
+   let map = {}
 
-    for (let s of strs) {
-        let key = s.split('').sort().join('');
-        if (!ans[key]) {
-            ans[key] = [];
+   for(let c of strs){
+        let a = c.split('').sort().join('')
+        if(!map[a]){
+            map[a] = []
         }
-        ans[key].push(s);
-    }
-
-    return Object.values(ans);    
+        map[a].push(c)
+   }
+   return Object.values(map)
 };
