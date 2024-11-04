7be9955f10277650380e6ce3700ee4da87217344
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-11-04 18:00:37 +0530

    Time: 0 ms (100%), Space: 48.2 MB (97.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,32 +3,31 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    let len = needle.length
+var strStr = function (haystack, needle) {
     let left = 0
-    let right = left + needle.length
+    let right = needle.length
 
-    if(haystack.length < needle.length){
+    if (haystack.length < needle.length) {
         return -1
     }
 
-    if(haystack == needle){
-return 0
+    if (haystack == needle) {
+        return 0
     }
 
-while(right <= haystack.length){
-    let substr = haystack.substring(left,right)
+    while (right <= haystack.length) {
+        let substr = haystack.substring(left, right)
 
 
-    if(substr == needle){
-        return left
-    }else{
-left++
-right++
-       
+        if (substr == needle) {
+            return left
+        } else {
+            left++
+            right++
+
+        }
+
     }
-
-}
 
 
     return -1
