dd0a60dfc186c6aeed4916d11206e37899c836c8
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-11-04 17:59:41 +0530

    Time: 41 ms (5.01%), Space: 54.4 MB (5.01%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let len = needle.length
+    let left = 0
+    let right = left + needle.length
+
+    if(haystack.length < needle.length){
+        return -1
+    }
+
+    if(haystack == needle){
+return 0
+    }
+
+while(right <= haystack.length){
+    let substr = haystack.substring(left,right)
+
+console.log(substr)
+
+    if(substr == needle){
+        return left
+    }else{
+left++
+right++
+       
+    }
+
+}
+
+
+    return -1
+
+
+
+};
