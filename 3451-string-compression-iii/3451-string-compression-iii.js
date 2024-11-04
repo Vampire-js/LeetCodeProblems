d8a2ee28826262b35a29bd080a483fae6695627f
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2024-11-04 17:05:23 +0530

    Time: 56 ms (25%), Space: 64.2 MB (34.29%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} word
+ * @return {string}
+ */
+var compressedString = function(word) {
+    let comp = ""
+
+    let left = 0
+    let right = 0
+
+     let char = word[left]
+        let count = 0
+    
+    while(right <= word.length){
+       
+        if(word[left] == word[right]){
+            if(count < 9){
+            right++
+            count++
+            }else{
+                comp += `${count}${char}`
+            count = 0
+            char = word[right]
+            left = right
+            }
+          
+        }else{
+            comp += `${count}${char}`
+            count = 0
+            char = word[right]
+            left = right
+        }
+    }
+
+    return comp
+};
