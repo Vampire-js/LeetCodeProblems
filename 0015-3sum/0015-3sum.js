cf4cc57e63c28bbb0efd63c52789e8a4ddfd38a6
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-01-11 16:14:07 +0530

    Time: 34 ms (76.44%), Space: 65.5 MB (70.54%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,36 +1,30 @@
-function twoSum(target, arr, start) {
-    let ans = [];
-    let seen = new Set();
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let ans = [];
 
-    for (let j = start; j < arr.length; j++) {
-        let complement = target - arr[j];
-        if (seen.has(complement)) {
-            ans.push([complement, arr[j]]);
-            while (j + 1 < arr.length && arr[j] === arr[j + 1]) {
-                j++;
-            }
-        }
-        seen.add(arr[j]);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+
+        while (left < right && nums[left] === nums[left - 1]) left++;
+        while (left < right && nums[right] === nums[right + 1]) right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
     }
+  }
 
-    return ans;
-}
-
-var threeSum = function(nums) {
-    let ans = [];
-    nums.sort((a, b) => a - b);
-
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-
-        let target = -nums[i];
-        let pairs = twoSum(target, nums, i + 1);
-
-        for (let pair of pairs) {
-            ans.push([nums[i], ...pair]);
-        }
-    }
-
-    return ans;
+  return ans;
 };
-
