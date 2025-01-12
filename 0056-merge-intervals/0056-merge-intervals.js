2f59591cd2830d8385b40b531d5011094dacf18d
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-01-12 09:03:05 +0530

    Time: 7 ms (82.08%), Space: 57.5 MB (94.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -1,12 +1,19 @@
 var merge = function(intervals) {
+
     intervals.sort((a, b) => a[0] - b[0]);
-    let ans = [];
-    for (let i = 0; i < intervals.length; i++) {
-        if (ans.length === 0 || ans[ans.length - 1][1] < intervals[i][0]) {
-            ans.push(intervals[i]);
+    const merged = [];
+    let prev = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        let interval = intervals[i];
+        if (interval[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], interval[1]);
         } else {
-            ans[ans.length - 1][1] = Math.max(ans[ans.length - 1][1], intervals[i][1]);
+            merged.push(prev);
+            prev = interval;
         }
     }
-    return ans;
+
+    merged.push(prev);
+    return merged;    
 };
