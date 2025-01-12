864a7e27acc7478f7bdbe33b3d753652354d1d32
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-01-12 08:01:58 +0530

    Time: 317 ms (6.09%), Space: 85.3 MB (8.75%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.js
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -5,8 +5,7 @@
 var maximumGap = function(nums) {
   if(nums.length < 2) return 0  
 
-function bucketSort(nums) {
-  const minVal = Math.min(...nums);
+ const minVal = Math.min(...nums);
   const maxVal = Math.max(...nums);
   const n = nums.length;
   const bucketSize = Math.max(1, Math.floor((maxVal - minVal) / (n - 1)));
@@ -25,10 +24,7 @@ function bucketSort(nums) {
     if (bucket.max !== -Infinity && bucket.max !== bucket.min) sorted.push(bucket.max);
   }
 
-  return sorted;
-}
 
-let sorted = bucketSort(nums)
 let largest = 0
 for(let i=0; i<sorted.length-1; i++){
     largest = Math.max(sorted[i+1]-sorted[i], largest)
