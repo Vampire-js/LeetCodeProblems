5b6e5efb83d259b65df2d3f949b0193ff5802668
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-01-16 14:49:27 +0530

    Time: 6 ms (42.58%), Space: 52.2 MB (46.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -4,20 +4,15 @@
  */
 var majorityElement = function (nums) {
     nums.sort((a, b) => a - b)
-
     let ans = []
     let left = 0
     let right = 0
     while (nums[right] != null) {
         while (nums[right] == nums[left]){
             right++
-        } 
-
+        }
         if (right - left > nums.length / 3) ans.push(nums[left])
-
         left = right
     }
-
     return ans
-
 };
