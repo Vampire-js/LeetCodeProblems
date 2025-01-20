79c290d52aad7d7cb83cec1094b03f83f86c8605
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-01-20 21:23:01 +0530

    Time: 19 ms (22.21%), Space: 51 MB (69.55%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function absoluteMin(a, b) {
+    if (a === 0 || b === 0) return 0;
+    return Math.abs(a) < Math.abs(b) ? a : b;
+}
+
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestGap = Infinity; 
+    let left = 0;
+    let right = nums.length - 1;
+
+for(left = 0; left < nums.length -2; left++){
+        let middle = left + 1;
+        right = nums.length -1
+
+        while (middle < right) {
+            const currentSum = nums[left] + nums[middle] + nums[right];
+            const currentGap = target - currentSum;
+
+            closestGap = absoluteMin(closestGap, currentGap);
+              if (Math.abs(currentSum - target) < Math.abs(closestGap - target)) closest = currentSum;
+            if (currentSum > target) right--;
+            else middle++;
+        }
+     
+    }
+
+    return target -closestGap;
+};
