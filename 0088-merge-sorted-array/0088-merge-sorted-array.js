c8eb8d9c3ac9b088be56f576471a3b40551f1f8f
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-02-18 23:06:58 +0530

    Time: 6 ms (6.62%), Space: 54.7 MB (5.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,9 +5,19 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
+
 var merge = function(nums1, m, nums2, n) {
     for(let i=m; i< m+n; i++){
         nums1[i] = nums2[i-m]
     }
-    nums1.sort((a,b) => (a-b))
+    for(let i=0; i<m+n; i++){
+        for(let j=0; j<m+n-i; j++){
+            if(nums1[j-1] >= nums1[j]){
+                let temp = nums1[j]
+                nums1[j] = nums1[j-1]
+                nums1[j-1] = temp
+            }
+        }
+    }
 };
