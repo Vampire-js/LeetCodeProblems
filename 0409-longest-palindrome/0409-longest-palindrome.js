aa64afb243045721b401c2d8e1bafc48c71d80dd
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-02-24 22:21:46 +0530

    Time: 5 ms (61.95%), Space: 54.5 MB (7.72%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let map = {}
+    let oneOdd = false
+    let maxOdd = 0
+    let max = 0
+    for(let c of s){
+        map[c] = 1 + (map[c] | 0)
+         if(map[c]%2 == 0 ) max += 2
+    }
+    
+    return s.length > max ? max+1 : max
+};
