bf55d2db047faaa938fdb788e009a4a56b07aeca
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-02-24 18:59:49 +0530

    Time: 10 ms (83.21%), Space: 66.2 MB (13.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,9 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  nums.sort((a,b) => a-b)
-  for(let i=0; i<nums.length-1; i++){
-    if(nums[i] == nums[i+1]) return true
+  let set = new Set()
+  for(let i=0; i<nums.length; i++){
+    if(set.has(nums[i])) return true
+   set.add(nums[i])
   }
   return false
 };
