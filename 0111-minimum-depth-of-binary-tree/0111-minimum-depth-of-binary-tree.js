6f1b0c4a9876ad2e951bee3aa262a12de626e3fa
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-02-25 17:36:20 +0530

    Time: 9 ms (23.91%), Space: 96.3 MB (5.02%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDepth = function(root) {
+    if(root == null) return 0
+    let ans = Infinity;
+   function min(r,d){
+    if(r == null) return
+    if(r.left == null && r.right == null){
+    ans = Math.min(ans, d)
+    }
+    min(r.left,d+1)
+    min(r.right,d+1)
+ }
+
+ min(root,1)
+ return ans
+};
