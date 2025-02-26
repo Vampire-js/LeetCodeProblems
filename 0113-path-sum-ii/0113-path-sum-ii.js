f49fdc0ad1c1a5d0e0ebcea53ea8556ae7a021bf
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-02-26 22:18:01 +0530

    Time: 0 ms (100%), Space: 57 MB (43.47%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.js
+++ 0113-path-sum-ii/0113-path-sum-ii.js
@@ -1,23 +1,24 @@
-var pathSum = function(root, targetSum) {
+var pathSum = function (root, targetSum) {
     let ans = [];
-    
+
     function sum(r, s, a) {
         if (r == null) return;
-        
+
         s += r.val;
-        a = [...a, r.val]; 
-        
+        a.push(r.val); 
+
         if (r.left == null && r.right == null) {
             if (s == targetSum) ans.push([...a]);
-            return;
-        }
-        
+        }else{
+
         sum(r.left, s, a);
         sum(r.right, s, a);
+        }
 
         a.pop()
-    }  
-    
+ 
+    }
+
     sum(root, 0, []);
     return ans;
 };
