af46817dfd2ca4ceeacba21ddca4b8a92ee857b0
Vampire.js <103945371+Vampire-js@users.noreply.github.com>
2025-03-04 17:51:37 +0530

    Time: 0 ms (100%), Space: 59.5 MB (21.52%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -5,9 +5,13 @@
 var reverseString = function(s) {
     let left = 0
     let right = s.length -1
+    let temp
 
     while(left < right){
-        [s[left], s[right]] = [s[right], s[left]]
+        temp = s[right]
+        s[right] = s[left]
+        s[left] = temp
+        
         left++
         right--
     }
